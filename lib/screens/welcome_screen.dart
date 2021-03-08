cff213f2549188d4dd40a1c8ad90b07f3504ae63
hisnameispum <sirasitxp@gmail.com>
2021-03-08 00:57:57 -0800

    Migrated to web

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,13 +1,12 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:horizontal_card_pager/card_item.dart';
+import 'package:horizontal_card_pager/horizontal_card_pager.dart';
 import 'package:intl/intl.dart';
 import 'package:nurtureland/screens/timer_screen.dart';
 import 'package:flutter_circular_slider/flutter_circular_slider.dart';
 import 'package:nurtureland/Models/minutes.dart';
 import 'package:nurtureland/widgets/tasks_list.dart';
-import 'package:flutter_cube/flutter_cube.dart' as cube;
-import 'package:scroll_snap_list/scroll_snap_list.dart';
-
+import 'package:flutter_cube/flutter_cube.dart';
 
 
 class MyHomePage extends StatefulWidget {
@@ -16,9 +15,49 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<int> data = [];
-  int _focusedIndex = 0;
-  GlobalKey<ScrollSnapListState> sslKey = GlobalKey();
+  List<CardItem> items = [
+    IconTitleCardItem(
+      text: "Mon 1",
+      iconData: Icons.timer,
+      selectedBgColor: Colors.green,
+    ),
+    IconTitleCardItem(
+      text: "Tue 2",
+      iconData: Icons.add,
+      selectedBgColor: Colors.green,
+
+    ),
+    IconTitleCardItem(
+      text: "Wed 3",
+      iconData: Icons.add_call,
+      selectedBgColor: Colors.green,
+
+    ),
+    IconTitleCardItem(
+      text: "Thu 4",
+      iconData: Icons.wifi,
+      selectedBgColor: Colors.green,
+
+    ),
+    IconTitleCardItem(
+      text: "Fri 5",
+      iconData: Icons.attach_file,
+      selectedBgColor: Colors.green,
+
+    ),
+    IconTitleCardItem(
+      text: "Sat 6",
+      iconData: Icons.airplay,
+      selectedBgColor: Colors.green,
+
+    ),
+    IconTitleCardItem(
+      text: "Sun 7",
+      iconData: Icons.airplay,
+      selectedBgColor: Colors.green,
+
+    ),
+  ];
 
   Color gradientStart = Colors.green[300];
   Color gradientEnd = Colors.yellow[200];
@@ -39,44 +78,7 @@ class _MyHomePageState extends State<MyHomePage> {
     // TODO: implement initState
     super.initState();
     selectedMinute = Minutes(_minutes);
-    for (int i = 0; i < 30; i++) {
-      data.add(Random().nextInt(100) + 1);
-    }
   }
-  void _onItemFocus(int index) {
-    setState(() {
-      _focusedIndex = index;
-    });
-  }
-  Widget _buildItemDetail() {
-    if (data.length > _focusedIndex)
-      return Container(
-        height: 350,
-        child: Text("index $_focusedIndex: ${data[_focusedIndex]}"),
-      );
-
-    return Container(
-      height: 350,
-      child: Text("No Data"),
-    );
-  }
-  Widget _buildListItem(BuildContext context, int index) {
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 5),
-      width: 350,
-      height: 100,
-      child: Material(
-        child: InkWell(
-            onTap: (){
-              sslKey.currentState.focusToItem(index);
-            },
-            focusColor: Colors.green,
-            child: Text("Child index $index"),
-          ),
-      ),
-    );
-  }
-
 
   @override
   Widget build(BuildContext context) {
@@ -146,7 +148,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   Padding(
                     padding: const EdgeInsets.only(left: 30, bottom: 30),
                     child: Text(
-                      "Goal: Get Good Grade.",
+                      "Goal: Get Things Done.",
                       style: TextStyle(
                         fontSize: 40,
                         fontWeight: FontWeight.bold,
@@ -158,18 +160,14 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ]),
           SizedBox(
-            height: 150,
+            height: 100,
             width: MediaQuery.of(context).size.width,
             child: Padding(
               padding: const EdgeInsets.only(top: 20,left: 10,right: 10),
-              child: ScrollSnapList(
-                margin: EdgeInsets.symmetric(vertical: 10),
-                onItemFocus: _onItemFocus,
-                itemSize: 360,
-                itemBuilder: _buildListItem,
-                itemCount: data.length,
-                key: sslKey,
-              ),
+              child: HorizontalCardPager(
+                onPageChanged: (page) => print("page : $page"),
+                onSelectedItem: (page) => print("selected : $page"),
+                items: items),
             ),
           ),
           Expanded(
@@ -198,9 +196,9 @@ class _MyHomePageState extends State<MyHomePage> {
             tileMode: TileMode.clamp),
       ),
       child: Center(
-        child: cube.Cube(
-          onSceneCreated: (cube.Scene scene) {
-            Object forest = cube.Object(fileName:'images/PUSHILIN_pine_tree.obj', scale: cube.Vector3(5.0, 5.0, 5.0));
+        child: Cube(
+          onSceneCreated: (Scene scene) {
+            Object forest = Object(fileName:'images/PUSHILIN_pine_tree.obj', scale: Vector3(5.0, 5.0, 5.0));
             scene.world.add(forest);
 
 
