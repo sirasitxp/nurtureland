4f91662d19cddb4c3e1e7454b7f6f0af7450a603
sirasit <sirasit@siruxsolutions.com>
2021-06-29 12:15:18 -0700

    Added task delete functionality with Dismissible.

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -27,4 +27,9 @@ class TaskData extends ChangeNotifier {
     task.toggleDone();
     notifyListeners();
   }
+
+  void deleteTask(Task task) {
+    _tasks.remove(task);
+    notifyListeners();
+  }
 }
--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -11,12 +11,39 @@ class TasksList extends StatelessWidget {
         return ListView.builder(
           itemBuilder: (context, index) {
             final task = taskData.tasks[index];
-            return TaskTile(
-              taskTitle: task.name,
-              isChecked: task.isDone,
-              checkBoxCallback: (checkboxState) {
-                taskData.updateTask(task);
+            return Dismissible(
+              background: Container(
+                color: Colors.red,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 10),
+                      child: Text(
+                        'Delete',
+                        textAlign: TextAlign.right,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              key: Key(task.name),
+              onDismissed: (direction) {
+                // Remove the item from the data source.
+                taskData.deleteTask(task);
               },
+              child: TaskTile(
+                taskTitle: task.name,
+                isChecked: task.isDone,
+                checkBoxCallback: (checkboxState) {
+                  taskData.updateTask(task);
+                },
+              ),
             );
           },
           itemCount: taskData.taskCount,
