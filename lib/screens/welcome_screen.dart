f0d7989018bc8a5bbba99c2b4375fa722c6bf860
sirasit <sirasit@siruxsolutions.com>
2021-07-18 02:41:46 -0700

    Added new tree

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:horizontal_card_pager/card_item.dart';
 import 'package:horizontal_card_pager/horizontal_card_pager.dart';
 import 'package:intl/intl.dart';
