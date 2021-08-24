116d30c8e587687c0a5a05b9393efb2e29359897
sirasit <sirasit@siruxsolutions.com>
2021-08-24 15:08:13 -0700

    Updated names

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -3,10 +3,26 @@ import 'package:flutter/cupertino.dart';
 class Task {
   String name;
   bool isDone;
+  int id;
+  DateTime dateTime;
 
-  Task({this.name, this.isDone = false});
+  Task({
+    this.name,
+    this.isDone = false,
+    this.id,
+    this.dateTime,
+  });
 
   void toggleDone() {
     isDone = !isDone;
+  }
+
+  Map<String, dynamic> toMap() {
+    return ({
+      'id': id,
+      'name': name,
+      'date': dateTime.toString(),
+      'isDone': isDone.toString(),
+    });
   }
 }
