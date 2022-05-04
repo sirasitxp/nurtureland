0c7bedaaa053b97bee62a881c9f42de037e31df7
sirasit <sirasit@siruxsolutions.com>
2022-05-04 16:53:39 -0700

    Added sign-in with google functionality

--- lib/screens/signin_screen.dart
+++ lib/screens/signin_screen.dart
@@ -1,11 +1,31 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class SignInScreen extends StatefulWidget {
   const SignInScreen({Key key}) : super(key: key);
 
   @override
   _SignInScreenState createState() => _SignInScreenState();
+}
+
+Future<UserCredential> signInWithGoogle() async {
+  // Trigger the authentication flow
+  final GoogleSignInAccount googleUser = await GoogleSignIn().signIn();
+
+  // Obtain the auth details from the request
+  final GoogleSignInAuthentication googleAuth =
+      await googleUser?.authentication;
+
+  // Create a new credential
+  final credential = GoogleAuthProvider.credential(
+    accessToken: googleAuth?.accessToken,
+    idToken: googleAuth?.idToken,
+  );
+
+  // Once signed in, return the UserCredential
+  return await FirebaseAuth.instance.signInWithCredential(credential);
 }
 
 class _SignInScreenState extends State<SignInScreen> {
@@ -55,7 +75,10 @@ class _SignInScreenState extends State<SignInScreen> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 IconButton(
-                  onPressed: () {},
+                  onPressed: () async {
+                    await signInWithGoogle();
+                    setState(() {});
+                  },
                   icon: SvgPicture.asset(
                     'images/google-original.svg',
                   ),
